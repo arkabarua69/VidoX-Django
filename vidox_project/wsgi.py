8b81eb02fe8b1e525ffedcb0b7f5889f9eea514c
Mac GunJon <155982830+arkabarua69@users.noreply.github.com>
2025-12-10 04:17:38 +0600

    update2

--- vidox_project/wsgi.py
+++ vidox_project/wsgi.py
@@ -2,15 +2,16 @@
 WSGI config for vidox_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vidox_project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vidox_project.settings")
 
 application = get_wsgi_application()
+
+# --- Vercel expects 'app' or 'handler' variable for Python serverless entry ---
+# Make the same WSGI application available under names Vercel looks for:
+app = application
+handler = application
